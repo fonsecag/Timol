f9d7b42609f168819e86fb3d62c293c7155cf09c
gregory.fonseca@uni.lu <gregory.fonseca@uni.lu>
2023-08-21 11:34:04 +0200

    npz files
    Features:
    - Now can read npz files

--- mol_reader.py
+++ mol_reader.py
@@ -3,23 +3,47 @@ from config import atom_colors, covalent_radii
 import numpy as np
 
 class MoleculeReader:
-
+    ase = False
+    npz = False
     def __init__(self, args):
         path = args.file
 
-        # self.molecules = ase.io.read(path, index = ":")
-        self.molecules = [ase.io.read(path, index = 0)]
+        if path.endswith('.npz'):
+            data = np.load(path)
+            self.molecules = [data['R'][0]]
+            self.z = data['z']
+            self.npz = True
+
+        else:
+            # self.molecules = ase.io.read(path, index = ":")
+            self.molecules = [ase.io.read(path, index = 0)]
+            self.ase = True
 
     def get_N(self):
         return len(self.molecules)
     
+    def get_atomic_numbers(self, index):
+        if self.ase:
+            atoms = self.molecules[index]
+            return  atoms.get_atomic_numbers()
+        
+        if self.npz:
+            return self.z
+        
+    def get_positions(self, index):
+        if self.ase:
+            atoms = self.molecules[index]
+            return atoms.get_positions()
+        
+        if self.npz:
+            return self.molecules[index]      
+
     def get_spheres(self, index = 0):
-        atoms = self.molecules[index]
-        z = atoms.get_atomic_numbers()
+        z = self.get_atomic_numbers(index)
         
         radii = covalent_radii[z]
         colors = [f'{c[0]};{c[1]};{c[2]}' for c in atom_colors[z]]
-        r = atoms.get_positions()
+        r = self.get_positions(index)
         r -= np.mean(r, axis = 0)
 
         return r, radii, colors
